2ce9fbfc3a7a33f42ffd2cd2b08535f8e037e00e
Chikwa235 <chisapachikwa25@gmail.com>
2025-07-20 19:39:00 +0200

    chore: get references to DOM elements for search, cards, and dark mode toggle

---
+++ script.js
@@ -0,0 +1,5 @@
+// Get references to DOM elements
+const searchInput = document.getElementById('searchInput');
+const cards = document.querySelectorAll('.card');
+const noResultsMessage = document.getElementById('noResultsMessage');
+const toggleBtn = document.getElementById('toggleMode');
